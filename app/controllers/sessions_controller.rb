0d4a4c1d6e207b485e605d7ff7ab365391187996
Oladejo Olakareem <oladejoolakareem@Oladejos-MacBook-Pro.local>
2017-12-12 13:45:43 -0800

    Write actions for sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,20 @@
 class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:email])
+    if user.save! && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to user_path(user)
+    else
+      flash.now.alert = 'Email or password is invalid'
+      render :new
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_url
+  end
 end
