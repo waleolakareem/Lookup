196a78c1bbbb227fa1773231d04c92a459609cc7
Oladejo Olakareem <oladejoolakareem@Oladejos-MacBook-Pro.local>
2017-12-12 15:20:26 -0800

    Redirect user to business_new after sign in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:email])
     if user.save! && user.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect_to user_path(user)
+      redirect_to new_business_path(user)
     else
       flash.now.alert = 'Email or password is invalid'
       render :new
