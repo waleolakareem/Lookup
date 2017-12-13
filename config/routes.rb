6c257e99b7f55da6f9f4499972e9c213c66068bc
Oladejo Olakareem <oladejoolakareem@Oladejos-MacBook-Pro.local>
2017-12-12 17:24:09 -0800

    Create reservation controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :sessions
   resources :users
-  resources :businesses
+  resources :businesses do
+    resources :reservations, shallow: true
+  end
   get 'logout', to: 'sessions#destroy'
   root 'users#new'
 end
