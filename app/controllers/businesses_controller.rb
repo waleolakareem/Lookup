57c801c87d957994a2142cbfafdfc648c012ea53
Oladejo Olakareem <oladejoolakareem@Oladejos-MacBook-Pro.local>
2017-12-13 11:57:05 -0800

    Delete unwanted spaces

--- app/controllers/businesses_controller.rb
+++ app/controllers/businesses_controller.rb
@@ -8,7 +8,6 @@ class BusinessesController < ApplicationController
   end
 
   def create
-
     @business = Business.where(term: params[:business][:term])
     @term = params[:business][:term]
     if @business.length <= 0
