85d4a7e061405ac6fdaa953ff6beb1289d6a267a
Oladejo Olakareem <oladejoolakareem@Oladejos-MacBook-Pro.local>
2017-12-25 22:03:40 -0600

    Debug strf error, found it was date picker

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,17 +1,3 @@
-# == Schema Information
-#
-# Table name: reservations
-#
-#  id               :integer          not null, primary key
-#  start_time       :datetime
-#  date             :date
-#  name_of_reserver :string
-#  number_of_seat   :integer
-#  business_id      :integer
-#  user_id          :integer
-#  created_at       :datetime         not null
-#  updated_at       :datetime         not null
-#
 
 class Reservation < ApplicationRecord
   belongs_to :user
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,3 @@
-# == Schema Information
-#
-# Table name: users
-#
-#  id              :integer          not null, primary key
-#  email           :string
-#  password_digest :string
-#  user_name       :string
-#  created_at      :datetime         not null
-#  updated_at      :datetime         not null
-#
 
 class User < ApplicationRecord
   validates_email_format_of :email, :message => 'is not looking good'
@@ -16,8 +5,4 @@ class User < ApplicationRecord
   has_many :reservations
   has_many :businesses, through: :reservations
 end
-# This file is used by Rack-based servers to start the application.
-# config.ru
-# require_relative 'config/environment'
 
-# run Rails.application
